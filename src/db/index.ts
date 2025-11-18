9ebbecda025cfcab8b6189037adfcbc2799e65a0
Pavol Repisky <pavol.repisky@votegroup.de>
2025-11-18 19:37:32 +0100

    feat(db): setup Drizzle ORM with Neon database

---
+++ src/db/index.ts
@@ -0,0 +1,9 @@
+import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/neon-http";
+
+config({ path: ".env" });
+
+const sql = neon(process.env.DATABASE_URL as string);
+
+export const db = drizzle({ client: sql });
